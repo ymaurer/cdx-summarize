ce51d64d082f6c3251ceb55052da3cd5c8591983
Yves Maurer <>
2021-03-09 14:01:10 +0100

    find overlap between n files generated by host_year_total.py

---
+++ overlap.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python3
+from argparse import ArgumentParser
+import sys
+
+COLUMN_INDEX    = 0
+COLUMN_FILENAME = 1
+COLUMN_FILEPTR  = 2
+COLUMN_KEY      = 3
+COLUMN_LINE     = 4
+COLUMN_YEAR     = 5
+
+
+RES_COUNT    = 0
+RES_N        = 1
+RES_SIZE     = 2
+
+def print_to_stderr(*a): 
+    print(*a, file = sys.stderr)
+
+def dowork(args):
+    fd = []
+    if args.noyear:
+        nkey = 1
+    else:
+        nkey = 2
+    all_eof = True
+    i = 0
+    for f in args.file:
+        fp = open(f, 'r')
+        l = fp.readline()
+        if not l == '':
+            all_eof = False
+            splitline = l.split(args.sep)
+            k = args.sep.join(splitline[0:nkey])
+            if args.noyear:
+                fd.append([i, f, fp, k, l])
+            else:
+                fd.append([i, f, fp, k, l, int(splitline[1])])
+        i = i + 1
+    # sort the files by their key
+    fd.sort(key=lambda k:k[COLUMN_KEY])
+    res = {}
+    lastkey = ''
+    while not all_eof:
+        # first find out how many files have the same key on the current line
+        nequal = 1
+        keyfound = {fd[0][COLUMN_INDEX]:0}
+        while nequal < len(fd) and fd[nequal][COLUMN_KEY] == fd[0][COLUMN_KEY]:
+            keyfound[fd[nequal][COLUMN_INDEX]] = nequal
+            nequal = nequal + 1
+        # save the number of common lines for that file combination and their 2 last columns if needed
+        skeyfound = sorted(keyfound.keys())
+        reskey = '-'.join([str(x) for x in skeyfound])
+        if not args.noyear:
+            year = fd[0][COLUMN_YEAR]
+        if not reskey in res:
+            if args.noyear:
+                res[reskey] = [0] * (1 + (2 * nequal if not args.nototal else 0))
+            else:
+                res[reskey] = {}
+        if not args.noyear and not year in res[reskey]:
+            res[reskey][year] = [0] * (1 + (2 * nequal if not args.nototal else 0))
+        # if the key hasn't changed from the last line from the file(s), we don't need to increase the counter
+        if not fd[0][COLUMN_KEY] == lastkey:
+            if args.noyear:
+                res[reskey][RES_COUNT] += 1
+            else:
+                res[reskey][year][RES_COUNT] += 1
+        lastkey = fd[0][COLUMN_KEY]
+        # keep adding the sums of the last two columns if wanted
+        if not args.nototal:
+            i = 0
+            for k in skeyfound:
+                tmpline = fd[keyfound[k]][COLUMN_LINE].split(args.sep)
+                if args.noyear:
+                    res[reskey][RES_N + 2 * i] += int(tmpline[-2])
+                    res[reskey][RES_SIZE + 2 * i] += int(tmpline[-1])
+                else:
+                    res[reskey][year][RES_N + 2 * i] += int(tmpline[-2])
+                    res[reskey][year][RES_SIZE + 2 * i] += int(tmpline[-1])
+                i = i + 1
+        # read the next line for those files who had the same key
+        i = 0
+        while i < nequal and i < len(fd):
+            l = fd[i][COLUMN_FILEPTR].readline()
+            if l == '':
+                del fd[i]
+            else:
+                splitline = l.split(args.sep)
+                k = args.sep.join(splitline[0:nkey])
+                fd[i][COLUMN_KEY] = k
+                fd[i][COLUMN_LINE] = l
+                if not args.noyear:
+                    fd[i][COLUMN_YEAR] = int(splitline[1])
+                i = i + 1
+        if len(fd) == 0:
+            all_eof = True
+        else:
+            fd.sort(key=lambda k:k[COLUMN_KEY])
+    for k in res:
+        print(k, res[k])
+
+parser = ArgumentParser(description='Find the overlap between several sorted files and optionally sum last two columns')
+parser.add_argument('-sep', default=' ', help='field separator (default is space)')
+parser.add_argument('-noyear', action="store_true", help='input file does not have a year column')
+parser.add_argument('-nototal', action="store_true", help='do not sum the last two columns')
+parser.add_argument('file', nargs='*', help='file (can be several)')
+args = parser.parse_args()
+dowork(args)

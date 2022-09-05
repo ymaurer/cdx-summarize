1e2a616a16be98792abca01229cc8c87c93ea234
Yves Maurer <>
2022-09-05 16:58:08 +0200

    took out parsing code to CdxParser

---
+++ CdxParser.py
@@ -0,0 +1,140 @@
+import re
+import json
+
+# CDX(J) formats supported
+FORMAT_UNKNOWN = 0
+FORMAT_CDX7 = 1
+FORMAT_CDXJ = 2
+FORMAT_CDXNbams = 3
+FORMAT_CDXNbamskrMSVg = 4
+
+class CdxParser:
+    # ----- Regular expressions for the CDX formats
+    # CDX N b a m s k r M S V g
+    # CDX format from openwayback with also the compressed size information
+    # N massaged url        surt
+    # b date                year + month
+    # a original url        scheme
+    # m mime type           mime
+    # s response code       status
+    # k checksum            unused
+    # r redirect            unused
+    # M meta tags           unused
+    # S comp. record size   size
+    # V file offset         unused
+    # g file name           unused
+    # surt: TLD,2D[,3D,4D])[/]
+    # \s+(?P<year>[0-9][0-9][0-9][0-9])(?P<month>[0-9][0-9])\d+\s+
+    reExprCDXNbamskrMSVg = r'^(?P<surt>([^ ):,]+)(,[^ ):,]+)*)(?:[):]\S*\s+|\s+)(?P<year>[0-9][0-9][0-9][0-9])(?P<month>[0-9][0-9])\d+\s+(?:(?P<scheme>https?)://)?\S+\s+(?P<mime>\S+)\s+(?P<status>\S+)\s+\S+\s+\S+\s+\S+\s+(?P<length>\S+)'
+
+    # CDX N b a m s (the N is not in surt format)
+    # CDX format used by openwayback historically. Additionnal fields are ignored
+    # N massaged URL        host
+    # b date                year + month
+    # a original url        scheme
+    # m mime type           mime
+    # s response code       status
+    reExprCDXNbams = r'^(?P<host>[^ /:]+)\S*\s+(?P<year>[0-9][0-9][0-9][0-9])(?P<month>[0-9][0-9])\d+\s+(?:(?P<scheme>https?)://)?\S+\s+(?P<mime>\S+)\s+(?P<status>\S+)'
+
+    # CDX N b a m s k S
+    # CDX7 format returned by Internet Archive's CDX server 
+    # N massaged url        surt
+    # b date                year + month
+    # a original url        scheme
+    # m mime type           mime
+    # s response code       status
+    # k checksum            unused
+    # S comp. record size   size
+    reExprCDX7 = r'^(?P<surt>([^ ):,]+)(,[^ ):,]+)*)(?:[):]\S*\s+|\s+)(?P<year>[0-9][0-9][0-9][0-9])(?P<month>[0-9][0-9])\d+\s+(?:(?P<scheme>https?)://)?\S+\s+(?P<mime>\S+)\s+(?P<status>\S+)\s+\S+\s+(?P<length>\S+)'
+
+    # CDXJ
+    # format used by pywb and returned by the common crawl
+    # N massaged url        surt
+    # b date                year + month
+    # JSON					data
+    reExprCDXJ = r'^(?P<surt>([^ ):,]+)(,[^ ):,]+)*)(?:[):]\S*\s+|\s+)(?P<year>[0-9][0-9][0-9][0-9])(?P<month>[0-9][0-9])\d+\s+(?P<data>.*$)'
+
+    def __init__(self, format, monthly=False, fullhost=False):
+        self.monthly = monthly
+        self.fullhost = fullhost
+        self.only200 = True
+        if format == FORMAT_CDX7:
+            self.re_line = re.compile(self.reExprCDX7)
+            self.has_json = False
+            self.surt = True
+        elif format == FORMAT_CDXJ:
+            self.re_line = re.compile(self.reExprCDXJ)
+            self.has_json = True
+            self.surt = True
+        elif format == FORMAT_CDXNbams:
+            self.re_line = re.compile(self.reExprCDXNbams)
+            self.has_json = False
+            self.surt = False
+        elif format == FORMAT_CDXNbamskrMSVg:
+            self.re_line = re.compile(self.reExprCDXNbamskrMSVg)
+            self.has_json = False
+            self.surt = True
+
+    def agg_from_surt(self, surt):
+        parts = surt.split(',')
+        if self.fullhost:
+            parts.reverse()
+            return '.'.join(parts)
+        else:
+            if len(parts) > 1:
+                return parts[1] + '.' + parts[0]
+            else:
+                return ''
+    
+    def lvl2_from_host(self, host):
+        p = host.rfind('.')
+        if p > -1:
+            p1 = host.rfind('.', 0, p -1)
+            return host[p1+1:]
+        return host
+
+    def parse_line(self, line):
+        m = self.re_line.match(line)
+        if m:
+            vars = m.groupdict()
+            if self.only200 and 'status' in vars and vars['status'][0:1] != '2':
+                return {}
+            ret = {}
+            # the date can be just YYYY or YYYYMM
+            if self.monthly:
+                retdate = int(vars['year'] + vars['month'])
+            else:
+                retdate = int(vars['year'])
+            # Aggregation key is either the second level domain or the full host
+            if self.surt:
+                retagg = self.agg_from_surt(vars['surt'])
+            else:
+                if self.fullhost:
+                    retagg = vars['host']
+                else:
+                    retagg = self.lvl2_from_host(vars['host'])
+            # line has embedded JSON
+            if self.has_json:
+                d = json.loads(vars['data'])
+                if self.only200 and d['status'] and d['status'][0:1] != '2':
+                    return {}
+                retsize = 0
+                if 'length' in d and d['length'].isnumeric():
+                    retsize = int(d['length'])
+                retscheme = ''
+                if len(d['url']) >= 8:
+                    if d['url'][0:7] == 'http://':
+                        retscheme = 'http'
+                    elif d['url'][0:8] == 'https://':
+                        retscheme = 'https'
+                retmime = 'unknown'
+                if 'mime' in d:
+                    retmime = d['mime']
+                return {'date' : retdate, 'agg' : retagg, 'scheme': retscheme, 'mime': retmime, 'length' : retsize}
+            else:
+                # line comes fully from the regex
+                retsize = 0
+                if 'length' in vars and vars['length'].isnumeric():
+                    retsize = int(vars['length'])
+                return {'date' : retdate, 'agg' : retagg, 'scheme': vars['scheme'], 'mime': vars['mime'], 'length' : retsize}
+        return {}
--- cdx-summarize.py
+++ cdx-summarize.py
@@ -6,191 +6,31 @@ import json
 import urllib
 import mime_counter
 import re
+import CdxParser
 
-# CDX(J) formats supported
-FORMAT_UNKNOWN = 0
-FORMAT_CDX7 = 1
-FORMAT_CDXJ = 2
-FORMAT_CDXNbams = 3
-FORMAT_CDXNbamskrMSVg = 4
 # Used to filter out invalid dates
 MIN_YEAR = 1991
 MAX_YEAR = 2022
-# Used to indicate End of String
-INT_MAX = 2147483647
 
-# Dictionary to hold the data per 2nd-level domain, per year
+# Dictionary to hold the data per domain, per year (or yearmonth)
 Hosts = {}
-
-# regular expression to signal the end of a host
-reURL = re.compile('(/|:|\?)')
 
 def print_to_stderr(*a): 
 	print(*a, file = sys.stderr)
 
-# the surt holds the inverted domain name as a sequence delimited by comma
-# so all we need to do is to go to the end of the domain or the second comma
-# and invert the parts
-def host_from_surt(surt, only2 = False):
-	p = surt.find(',')
-	if p == -1:
-		return ''
-	p2 = surt.find(')', p + 1)
-	if p2 == -1:
-		return ''
-	parts = surt[0:p2].split(',')
-	if only2:
-		if len(parts) > 1:
-			return parts[1] + '.' + parts[0]
-		else:
-			return parts[0]
-	else:
-		parts.reverse()
-		return '.'.join(parts)
-
-
-def lvl2_from_surt(surt):
-	return host_from_surt(surt, True)
-
-# the massaged URL has no protocol for http and https and strips the 'www'
-# so we look for the first non-host character 
-def host_from_massagedURL(url):
-    p = reURL.search(url)
-    if p:
-        return url[0:p.span(0)[0]]
-    else:
-	    return url
-
-def lvl2_from_massagedURL(url):
-	host = host_from_massagedURL(url)
-	p = host.rfind('.')
-	if p > -1:
-		p1 = host.rfind('.', 0, p -1)
-		return host[p1+1:]
-	return host
-	
-def year_from_date(date, ismonthly):
-	if len(date) < 6:
-		return -1
-	if ismonthly:
-		year = date[0:6]
-	else:
-		year = date[0:4]
-	if year.isdigit():
-		if ismonthly:
-			return int(year)
-		else:
-			yi = int(year)
-			if yi >= MIN_YEAR and yi <= MAX_YEAR:
-				return yi
-	return -1
-
-def scheme_from_url(url):
-	if len(url)<8:
-		return ''
-	if url[0:7] == 'http://':
-		return 'http'
-	elif url[0:8] == 'https://':
-		return 'https'
-	return ''
-
-# parse CDXJ file
-def parse_line_cdxj(line, ismonthly, fullhost=False):
-	tokens = line.split()
-	if len(tokens) < 3:
-		return ''
-	year = year_from_date(tokens[1], ismonthly)
-	if year == -1:
-		return ''
-	p = line.find(" {\"") + 1
-	p1 = line.rfind("\"}")
-	if p == -1:
-		return ''
-	try:
-		info = json.loads(line[p:p1+2])
-		if fullhost:
-			aggregate_by = host_from_surt(tokens[0])
-		else:
-			aggregate_by = lvl2_from_surt(tokens[0])
-		summarize_line(aggregate_by, year, info)
-		return aggregate_by
-	except Exception as inst:
-		print_to_stderr('cdxj: could not parse line', inst)
-	return ''
-
-# parse CDX7 file as returned by Internet Archive's CDX server by default
-def parse_line_cdx7(line, ismonthly, fullhost=False):
-	tokens = line.split()
-	if len(tokens) < 7:
-		return ''
-	year = year_from_date(tokens[1], ismonthly)
-	if year == -1:
-		return ''
-	if tokens[6] == '-':
-		tokens[6] = '0'
-	info = {"url": tokens[2], "mime": tokens[3],"status":tokens[4],"length":tokens[6]}
-	try:
-		if fullhost:
-			aggregate_by = host_from_surt(tokens[0])
-		else:
-			aggregate_by = lvl2_from_surt(tokens[0])
-		summarize_line(aggregate_by, year, info)
-		return aggregate_by
-	except Exception as inst:
-		print_to_stderr('cdx: could not parse line', inst)
-	return ''
-
-def parse_line_cdxNbams(line, ismonthly, fullhost=False):
-	tokens = line.split()
-	if len(tokens) < 5:
-		return ''
-	year = year_from_date(tokens[1], ismonthly)
-	if year == -1:
-		return ''
-	info = {"url": tokens[2], "mime": tokens[3],"status":tokens[4],"length":0}
-	try:
-		if fullhost:
-			aggregate_by = host_from_massagedURL(tokens[0])
-		else:
-			aggregate_by = lvl2_from_massagedURL(tokens[0])
-		summarize_line(aggregate_by, year, info)
-		return aggregate_by
-	except Exception as inst:
-		print_to_stderr('cdx: could not parse line', inst)
-	return ''
-
-def parse_line_cdxNbamskrMSVg(line, ismonthly, fullhost=False):
-	tokens = line.split()
-	if len(tokens) < 5:
-		return ''
-	year = year_from_date(tokens[1], ismonthly)
-	if year == -1:
-		return ''
-	info = {"url": tokens[2], "mime": tokens[3],"status":tokens[4],"length":tokens[8]}
-	try:
-		if fullhost:
-			aggregate_by = host_from_surt(tokens[0])
-		else:
-			aggregate_by = lvl2_from_surt(tokens[0])
-		summarize_line(aggregate_by, year, info)
-		return aggregate_by
-	except Exception as inst:
-		print_to_stderr('cdx: could not parse line', inst)
-	return ''	
-
-def summarize_line(lvl2, year, info):
-	if len(lvl2) < 1:
+def summarize_line(info):
+	if not 'agg' in info:
 		return
-	if "status" in info and info["status"][0:1] == "2":
-		scheme = scheme_from_url(info['url'])
-		if not lvl2 in Hosts:
-			Hosts[lvl2] = {}
-		if not year in Hosts[lvl2]:
-			Hosts[lvl2][year] = mime_counter.init_counter()
-		if not 'mime' in info:
-			info['mime']='unknown'
-		mime_counter.add_mime(Hosts[lvl2][year], info["mime"], 1, int(info["length"]))
-		mime_counter.add_scheme(Hosts[lvl2][year], scheme, 1, int(info["length"]))
+	agg = info['agg']
+	if not agg in Hosts:
+		Hosts[agg] = {}
+	date = info['date']
+	if not date in Hosts[agg]:
+		Hosts[agg][date] = mime_counter.init_counter()
+	if not 'mime' in info:
+		info['mime']='unknown'
+	mime_counter.add_mime(Hosts[agg][date], info["mime"], 1, info["length"])
+	mime_counter.add_scheme(Hosts[agg][date], info['scheme'], 1, info["length"])
 
 def output_results(args):
 	dict_items = Hosts.items()
@@ -227,66 +67,63 @@ def output_partial_results(args, agg_by):
 def determine_cdx_type(line):
 	tokens = line.split()
 	if len(tokens) < 3:
-		return FORMAT_UNKNOWN
+		return CdxParser.FORMAT_UNKNOWN
 	if len(tokens)>=6:
 		if line.strip() == 'CDX N b a m s k r M S V g':
-			return FORMAT_CDXNbamskrMSVg
+			return CdxParser.FORMAT_CDXNbamskrMSVg
 		if tokens[0]=='CDX':
 			if line[0:14] == ' CDX N b a m s':
-				return FORMAT_CDXNbams
+				return CdxParser.FORMAT_CDXNbams
 			else:
-				return FORMAT_UNKNOWN
+				return CdxParser.FORMAT_UNKNOWN
 	# all supported formats have the date as a 14 digit string in the second place
 	if (len(tokens[1]) != 14):
-		return FORMAT_UNKNOWN
+		return CdxParser.FORMAT_UNKNOWN
 	# check if the line ends with a "} indicative of JSON
 	if line.rstrip()[-2:] == '"}':
-		return FORMAT_CDXJ
+		return CdxParser.FORMAT_CDXJ
 	if len(tokens)==7:
-		return FORMAT_CDX7
+		return CdxParser.FORMAT_CDX7
 	elif len(tokens)==10:
-		return FORMAT_CDXNbams
-	return FORMAT_UNKNOWN
+		return CdxParser.FORMAT_CDXNbams
+	return CdxParser.FORMAT_UNKNOWN
 
 def cdx_type_from_args(args):
 	if args.format=='cdxj':
-		return FORMAT_CDXJ
+		return CdxParser.FORMAT_CDXJ
 	elif args.format=='cdx7':
-		return FORMAT_CDX7
+		return CdxParser.FORMAT_CDX7
 	elif args.format=='cdxNbams':
-		return FORMAT_CDXNbams
+		return CdxParser.FORMAT_CDXNbams
 	elif args.format=='cdxNbamskrMSVg':
-		return FORMAT_CDXNbamskrMSVg
+		return CdxParser.FORMAT_CDXNbamskrMSVg
 	else:
-		return FORMAT_UNKNOWN
+		return CdxParser.FORMAT_UNKNOWN
 
 def read_cdx_file(args, fil, filename):
 	line = fil.readline()
 	ftype = cdx_type_from_args(args)
-	if ftype == FORMAT_UNKNOWN:
+	if ftype == CdxParser.FORMAT_UNKNOWN:
 		ftype = determine_cdx_type(line)
-	if ftype == FORMAT_CDXJ:
-		func = parse_line_cdxj
-	elif ftype == FORMAT_CDX7:
-		func = parse_line_cdx7
-	elif ftype == FORMAT_CDXNbams:
-		func = parse_line_cdxNbams
-	elif ftype == FORMAT_CDXNbamskrMSVg:
-		func = parse_line_cdxNbamskrMSVg
-	else:
+	if ftype == CdxParser.FORMAT_UNKNOWN:
 		print_to_stderr("Unsupported cdx format: ", filename, line)
 		return
-	aggregate_by = func(line.rstrip(), args.monthly, args.fullhost)
+	parser = CdxParser.CdxParser(ftype, args.monthly, args.fullhost)
+	aggregate_by = ''
+	res = parser.parse_line(line)
+	if 'agg' in res:
+		aggregate_by = res['agg']
+		summarize_line(res)
 	for line in fil:
 		try:
-			agg = func(line.rstrip(), args.monthly, args.fullhost)
+			res = parser.parse_line(line)
+			summarize_line(res)
 			if args.assume_unique:
-				if agg != aggregate_by:
+				if 'agg' in res and res['agg'] != aggregate_by:
 					output_partial_results(args, aggregate_by)
-					aggregate_by = agg
+					aggregate_by = res['agg']
 		except Exception as inst:
 			print_to_stderr("Unexpected error:", filename, inst, line)
-
 
 def dowork(args):
 	for f in args.file:
@@ -301,7 +138,7 @@ def dowork(args):
 				with open(f, 'r', encoding=args.encoding) as fil:
 					read_cdx_file(args, fil, f)
 			except Exception as inst:
-				print_to_stderr("Error", inst, f)
+				print_to_stderr("Error (dowork)", inst, f)
 	output_results(args)
 
 

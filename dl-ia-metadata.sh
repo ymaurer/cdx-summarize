284652682fd8c68d38aa0eb13efde55c702cc2de
Yves Maurer <>
2022-09-29 11:23:55 +0200

    cleanup

--- dl-ia-metadata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -ne 1 ]; then
-	echo "usage: dl-ia-metadata.sh TLD"
-	echo "\tTLD is a top-level domain"
-	exit
-fi
-
-curl -Ls "https://web.archive.org/__wb/search/metadata?q=tld:$1" | jq -S .
